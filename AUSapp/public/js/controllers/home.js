a1071aeb010a6d93738dc321ff9b41f18973c311
Loran Briggs <loranbriggs@gmail.com>
2014-01-23 19:51:36 -0800

    fix for firefox

--- AUSapp/public/js/controllers/home.js
+++ AUSapp/public/js/controllers/home.js
@@ -7,12 +7,12 @@ angular.module('AUSapp').controller('Home', ['$scope', function($scope) {
 
   $scope.getLocation = function() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(GetLocation);
       function GetLocation(location) {
           $scope.lat = location.coords.latitude;
           $scope.long = location.coords.longitude;
           $scope.$apply();
       }
+      navigator.geolocation.getCurrentPosition(GetLocation);
     }
     else{x.innerHTML="Geolocation is not supported by this browser.";}
   }
