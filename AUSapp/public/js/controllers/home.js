10936c496e88d2f78a723efa6849a6bbf8d9bb85
Loran Briggs <loranbriggs@gmail.com>
2014-01-23 20:43:50 -0800

    added sockjs

--- AUSapp/public/js/controllers/home.js
+++ AUSapp/public/js/controllers/home.js
@@ -1,22 +1,32 @@
 angular.module('AUSapp').controller('Home', ['$scope', function($scope) {
 
 
+  $scope.sock = new SockJS('/');
+  $scope.messages = [];
+  $scope.name = "";
   $scope.latitude = "";
   $scope.longitude = "";
 
   $scope.init = function() {
+    $scope.name = prompt("Please enter you're name.", "Ada");
     if (navigator.geolocation) {
       function updateLocation(lati, longi) {
-          $scope.latitude = lati;
-          $scope.longitude = longi;
-          $scope.$apply();
+        $scope.latitude = lati;
+        $scope.longitude = longi;
+        $scope.$apply();
       }
       var watchPOS = navigator.geolocation.watchPosition(function(position) {
         updateLocation(position.coords.latitude, position.coords.longitude);
-        console.log("watch");
+        $scope.$apply();
+        $scope.sock.send($scope.name + ": " + $scope.latitude + " / " + $scope.longitude);
       });
     }
     else{alert("Geolocation is not supported by this browser.");}
   }
+
+  $scope.sock.onmessage = function(e) {
+    $scope.messages.push(e.data);
+    $scope.$apply();
+  };
 
 }]);
