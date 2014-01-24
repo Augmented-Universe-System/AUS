61dc0fe3dde43219a00ebe86c4cb62623fac4be7
Loran Briggs <loranbriggs@gmail.com>
2014-01-23 19:01:18 -0800

    get location on home page

--- AUSapp/public/js/controllers/home.js
+++ AUSapp/public/js/controllers/home.js
@@ -2,8 +2,19 @@ angular.module('AUSapp').controller('Home', ['$scope', function($scope) {
 
   $scope.init = function() {}
 
-  $scope.buttonPress = function() {
-    alert("Fire!");
-  };
+  $scope.lat = "";
+  $scope.long = "";
+
+  $scope.getLocation = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(GetLocation);
+      function GetLocation(location) {
+          $scope.lat = location.coords.latitude;
+          $scope.long = location.coords.longitude;
+          $scope.$apply();
+      }
+    }
+    else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
 
 }]);
