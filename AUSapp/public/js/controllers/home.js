01cdd7dc8fb032d534df15ec2cfe886102956954
Pablo Settecase <paeblits@gmail.com>
2014-01-30 19:06:37 -0800

    Pablo: added canvas and fills a small rectangle based on position. First attempt.

--- AUSapp/public/js/controllers/home.js
+++ AUSapp/public/js/controllers/home.js
@@ -6,10 +6,20 @@ angular.module('AUSapp').controller('Home', ['$scope', function($scope) {
   $scope.name = "";
   $scope.latitude = "";
   $scope.longitude = "";
+  var canvas, ctx = "";
+
 
   $scope.init = function() {
+
+    canvas = document.getElementById('gameCanvas');
+    ctx = canvas.getContext('2d');
+
+    canvas.width = 200;
+    canvas.height = 150;
+    canvas.style.border = "1px solid";
+
     $scope.name = prompt("Please enter you're name.", "Ada");
-    /*
+  
     if (navigator.geolocation) {
       function updateLocation(lati, longi) {
         $scope.latitude = lati;
@@ -18,15 +28,25 @@ angular.module('AUSapp').controller('Home', ['$scope', function($scope) {
       }
       var watchPOS = navigator.geolocation.watchPosition(function(position) {
         updateLocation(position.coords.latitude, position.coords.longitude);
+
+        //ctx.clearRect(0,0, canvas.width, canvas.height);
+
+        var x = ($scope.latitude * 10000000) % 100;
+        var y = (Math.abs($scope.longitude) * 10000000) % 100;
+        ctx.fillRect(x, y, 5, 5);
+
         $scope.$apply();
-        $scope.sock.send($scope.name + ": " + $scope.latitude + " / " + $scope.longitude);
+        $scope.sock.send($scope.name + ": " + x + " / " + y) ;
       });
     }
     else{alert("Geolocation is not supported by this browser.");}
-    */
+    
   };
 
   $scope.sendMessage = function() {
+    // ctx.clearRect(0,0, canvas.width, canvas.height);
+    // ctx.fillRect($scope.latitude, $scope.longitude, 10, 10);
+
     $scope.sock.send($scope.name + ": " + $scope.latitude + " / " + $scope.longitude);
   };
 
