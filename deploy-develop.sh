6b58a75e3e63367969ee52fd7770413ad1f367d6
Loran Briggs <loranbriggs@gmail.com>
2014-01-30 19:38:35 -0800

    update deploy script

--- deploy-develop.sh
+++ deploy-develop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Attempting to deploy master branch at $(date)" >> /home/project/deployment_log.txt
+echo "Attempting to deploy develop branch at $(date)" >> /home/project/deployment_log.txt
 
 git pull
 
-forever restart oHxf
+forever restartall
